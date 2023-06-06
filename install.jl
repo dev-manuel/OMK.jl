3547e84470943623e2efc97603323cf067a47172
dev-manuel <manuel_krofficial@online.de>
2023-06-06 05:53:44 +0200

    ref: install script + project

---
+++ install.jl
@@ -0,0 +1,33 @@
+using Pkg
+
+
+# @info "Installing OM.jl"
+# @time include("OM.jl/install.jl")
+
+@info "Developing sub-packages"
+subPkgs = joinpath.(
+  pwd(),
+  [
+    "MKAbsyn.jl",
+    "OMKParser.jl",
+  ])
+
+
+for pkg in subPkgs
+  Pkg.develop(path=pkg)
+end
+
+@info "Developing OMK.jl"
+@time Pkg.develop(path=pwd())
+
+@info "The parser needs some external libraries. Build the parser"
+@time Pkg.build("OMKParser")
+
+@info "Installing auxilary dependencies"
+@time Pkg.add("Revise")
+
+@info "precompiling and running tests"
+@time include("run.jl")
+
+@info "Running tests again (but much faster)"
+@time include("run.jl")
